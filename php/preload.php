6e97bc6b34d6726709234e14047d19400ffc7a39
synesis901 <albert.uy901@gmail.com>
2015-05-31 18:08:37 -0600

    adding maps loader

--- php/preload.php
+++ php/preload.php
@@ -8,6 +8,7 @@
 	
 	 
 	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
-	<!--  <script src="js/gmaps.js"></script> -->
 	<script type="text/javascript" src="js/jQueryMaps/jquery.ui.map.js"></script>
 	<script type="text/javascript" src="js/jQueryMaps/jquery.ui.map.extensions.js"></script>
+	<script src="js/defaultloads.js"></script>
+	
