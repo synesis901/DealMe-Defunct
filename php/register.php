942387e7c1eb7eaa46ff8cf64756d4fa49aabac0
synesis901 <albert.uy901@gmail.com>
2015-06-07 16:57:32 -0600

    Revert "removed username"
    This reverts commit 7220ac1b386de61c73a47f0afc52f576ef4255a0.
    
    Conflicts:
    	php/bodystart.php

--- php/register.php
+++ php/register.php
@@ -7,12 +7,12 @@ include("Classes/Main/class.php");
 
 // Everything is preset. I will make changes based on how the info gets here from the signup form
 $UA = array();
-$UA['Password'] = isset($_POST['pass'])?$_POST['pass'] : "";
-$UA['Password2'] = isset($_POST['pass2'])?$_POST['pass2'] : "";
+$UA['Username'] = isset($_POST['username'])?$_POST['username'] : "";
+$UA['Password'] = isset($_POST['password'])?$_POST['password'] : "";
+$UA['Password2'] = isset($_POST['password2'])?$_POST['password2'] : "";
 $UA['Name'] = isset($_POST['name'])? $_POST['name']: "";
 $UA['Email'] = isset($_POST['email'])? $_POST['email'] : "";
 $UA['Email2'] = isset($_POST['email2'])? $_POST['email2']: "";
-$UA['Location'] = isset($_POST['location'])? $_POST['location']: "";
 
 $UAT = new m_UserAccount();
 $UAT->newuser($UA);
