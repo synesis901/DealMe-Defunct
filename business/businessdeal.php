49d5fe1a6286ad07db895204672ef52fd86c38f8
umsldragon <a1dotz9@yahoo.ca>
2015-06-21 01:46:18 -0600

    businessprofile page works
    queries work on businessdealphp

--- business/businessdeal.php
+++ business/businessdeal.php
@@ -1,9 +1,14 @@
 <?php
+	include ($_SERVER['DOCUMENT_ROOT']."/php/Classes/Main/class.php");
 	include ($_SERVER['DOCUMENT_ROOT']."/php/header.php");
 	echo "DealMe";
 	include ($_SERVER['DOCUMENT_ROOT']."/php/preload.php");
 	include ($_SERVER['DOCUMENT_ROOT']."/php/Classes/Search/c_search.php");
 	include ($_SERVER['DOCUMENT_ROOT']."/php/bodystart.php");
+	
+	include ($_SERVER['DOCUMENT_ROOT']."/php/businessDisplay.php");
+	
+	$content = new businessDisplay($_GET);
 	
 ?>
 
@@ -29,12 +34,16 @@
 		<div class="row">
 			<div class="col-md-12">
 				<div class="col-md-5">
-					<h1 class="page-header">RuralTech Computers, Inc.</h1>
+					<h1 class="page-header"><?php echo $content->getBinfo()['name']; ?></h1>
 					<div class="row">
-						<h4>Location: <small>32 23rd Street, Battleford SK</small></h4>
-						<h4>Phone: <small>123 123-4567</small></h4>
-						<h4>Email: <small>email@email.com</small></h4>
-						<p>Get two hours of hardware work done, get a free virus removal!</p>
+						<h4>Location: <small><?php echo $content->getBinfo()['full_address']; ?></small></h4>
+						<h4>Phone: <small><?php echo $content->getBinfo()['phone']; ?></small></h4>
+						<h4>Email: <small><?php echo $content->getBinfo()['email']; ?></small></h4>
+						<h4>WebSite: <small><?php echo $content->getBinfo()['url']; ?></small></h4>
+						<h4>Twitter: <small><?php echo $content->getBinfo()['twitter']; ?></small></h4>
+						<h4>FaceBook: <small><?php echo $content->getBinfo()['facebook']; ?></small></h4>
+						<h4>Instagram: <small><?php echo $content->getBinfo()['instagram']; ?></small></h4>
+						<p><?php echo $content->getBinfo()['description']; ?></p>
 					</div>
 					<div class="row">
 					</div>
@@ -45,8 +54,8 @@
 		</div>
 		<div class="row">
 			<div class="col-md-12">
-				<h1>Deals</h1>
-				<p>No Current Deals</p>
+				<h1>Deals</h1><!-- see /php/businessDisplay.php::line34 to change HTML -->
+				<?php echo $content->getDinfoByBusiness(); ?>
 			</div>
 			
 		</div>
---
+++ php/businessDisplay.php
@@ -0,0 +1,70 @@
+<?php
+/*
+	shows business profiles from db
+	security issue with _GET being stored directly to var
+	need to add date filters and active filters too
+*/
+
+class businessDisplay{
+	private $ids = array();
+	private $businessInfo;
+	private $dealInfoByGet;
+	private $dealInfoByBusiness = "<p>No Deals Currently</p>";// default display
+	
+	public function __construct($get){
+		$this->storeGET($get);
+		
+		$this->bQuery();
+		$this->dQueryByBusiness();
+		$this->dQueryByGet();
+	}
+	
+	private function storeGET($info){
+		$this->ids['b'] = isset($info['b'])?new MongoID($info['b']):"";//business MongoID
+		$this->ids['d'] = isset($info['d'])?new MongoID($info['d']):"";//deal MongoID
+	}
+	
+	private function bQuery(){
+		$this->businessInfo = m_FindOne::result(m_var::get_database(),m_var::get_businessAccount(),array("_id"=>$this->ids['b']));
+	}
+	
+	private function dQueryByBusiness(){
+		//lists all deals by current business ID
+		m_Find::result(m_var::get_database(),m_var::get_businessAds(),array("business_id"=>$this->ids['b']));
+		$result = m_Find::get_results();
+		
+		/*
+			---- HTML Bootstrap Content Here ----
+		*/
+		if($result){
+			$HTML="";
+			foreach($result as $doc){
+				if($doc['active'] == "true"){
+					$HTML .= 
+					'<h4>Name: <small>'.$doc['name'].'</small></h4>
+					<p>'.$doc['description'].'</p>
+					<h4><a href="businessdeal.php?d='.$doc['_id'].'">View Deal</a></h4>';
+				}
+			}
+			$this->dealInfoByBusiness = $HTML;
+		}
+	}
+	
+	private function dQueryByGet(){
+		$this->dealInfoByGet = m_FindOne::result(m_var::get_database(),m_var::get_businessAds(),array("_id"=>$this->ids['d']));
+	}
+	
+	public function getBinfo(){
+		return $this->businessInfo;
+	}
+	
+	public function getDinfo(){
+		return $this->dealInfoByGet;
+	}
+	
+	public function getDinfoByBusiness(){
+		return $this->dealInfoByBusiness;
+	}
+}
+
+?>
