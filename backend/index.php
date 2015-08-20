d3d73d79fef664704d2b36388bc294449035ecf9
umsldragon <a1dotz9@yahoo.ca>
2015-08-20 00:52:48 -0600

    tweak

--- backend/index.php
+++ backend/index.php
@@ -15,7 +15,8 @@
 			<form id="backend" class="form-inline">
 				<div class="row">
 					these are the input names I'm looking for in this form<br/>
-					name, full_address, city, province, description, modal_desc, url, twitter, facebook, instagram, phone, email
+					name, full_address, city, province, description, modal_desc, url, twitter, facebook, instagram, phone, email and tags(aka category. but this is an array)<br/>
+					Reference the Mongo DB via RoboMongo to see what categories and cities are available for searching
 				</div>
 				<div class="row">
 					<button type="submit" formmethod="post" formaction="<?php echo "/php/backend.php";?>" form="backend">Search Now</button>
